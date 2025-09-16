24023c4f083b09a3e84872df8af7cac0231ff18b
sao-coding <jeter.nice@gmail.com>
2025-09-17 02:06:10 +0800

    refactor: 調整全局樣式以改善背景透明度

--- src/app/(blog)/layout.tsx
+++ src/app/(blog)/layout.tsx
@@ -1,15 +1,15 @@
-import Header from "@/components/layout/header/header";
+import Header from '@/components/layout/header/header'
 
 export default function BlogLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <>
-      <div className="fixed z-[-999] inset-0 bg-[url('/img/index.webp')] before:content-[''] bg-cover bg-center bg-no-repeat before:absolute before:inset-0 before:bg-black/50" />
+      {/* <div className="fixed z-[-999] inset-0 bg-[url('/img/index.webp')] before:content-[''] bg-cover bg-center bg-no-repeat before:absolute before:inset-0 before:bg-black/50" /> */}
       <Header />
       {children}
     </>
-  );
+  )
 }
