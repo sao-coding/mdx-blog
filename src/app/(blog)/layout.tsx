f04a169666946173c3bfd7ca24baf8ad889b02c3
sao-coding <jeter.nice@gmail.com>
2025-10-02 01:04:59 +0800

    feat: 更新佈局以包含頁腳並調整內容填充樣式

--- src/app/(blog)/layout.tsx
+++ src/app/(blog)/layout.tsx
@@ -1,4 +1,5 @@
 import Header from '@/components/layout/header/header'
+import Footer from '@/components/layout/footer'
 
 export default function BlogLayout({
   children,
@@ -6,13 +7,14 @@ export default function BlogLayout({
   children: React.ReactNode
 }>) {
   return (
-    <div className="text-[14px]">
+    <div className="text-[14px] h-full">
       <Header />
-      <div className="pt-[4.5rem]">
+      <div className="pt-[4.5rem] fill-content">
         {/* <div className="fixed z-[-999] inset-0 bg-[url('/img/index.webp')] before:content-[''] bg-cover bg-center bg-no-repeat before:absolute before:inset-0 before:bg-black/50" /> */}
 
         {children}
       </div>
+      <Footer />
     </div>
   )
 }
