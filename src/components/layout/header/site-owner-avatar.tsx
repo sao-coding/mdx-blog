74b8c1b7f66b51d4b27ab143a18ea91ed9ec9955
sao-coding <jeter.nice@gmail.com>
2026-02-22 05:21:46 +0800

    style: 更新 SiteOwnerAvatar 元件的樣式類別，統一使用 size-10

--- src/components/layout/header/site-owner-avatar.tsx
+++ src/components/layout/header/site-owner-avatar.tsx
@@ -2,7 +2,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const SiteOwnerAvatar = () => {
   return (
-    <Avatar className="h-10 w-10">
+    <Avatar className="size-10">
       <AvatarImage src="/img/avatar.jpg" />
       <AvatarFallback>唯</AvatarFallback>
     </Avatar>
