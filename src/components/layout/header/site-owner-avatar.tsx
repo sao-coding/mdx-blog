75476e77cb911a8bdb3407220b7bc8f71395f446
sao-coding <jeter.nice@gmail.com>
2026-02-22 15:51:33 +0800

    feat: 將 SiteOwnerAvatar 包裝在 Link 元件中，實現回到首頁的功能

--- src/components/layout/header/site-owner-avatar.tsx
+++ src/components/layout/header/site-owner-avatar.tsx
@@ -1,11 +1,14 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Link from "next/link";
 
 const SiteOwnerAvatar = () => {
   return (
-    <Avatar className="size-10">
-      <AvatarImage src="/img/avatar.jpg" />
-      <AvatarFallback>唯</AvatarFallback>
-    </Avatar>
+    <Link href="/" aria-label="回到首頁">
+      <Avatar className="size-10">
+        <AvatarImage src="/img/avatar.jpg" />
+        <AvatarFallback>唯</AvatarFallback>
+      </Avatar>
+    </Link>
   );
 };
 
